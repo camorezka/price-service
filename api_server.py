1dbe6175fc3977146e531f1490331145098762b8
camorezka <sadovoyura777333arbuz@gmail.com>
2026-06-01 14:51:15 +0300

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -24,4 +24,5 @@ async def add_task(task: MonitorTask):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("api_server:app", host="0.0.0.0", port=port)
